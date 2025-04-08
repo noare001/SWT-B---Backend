5292c5bf2b37c0ad7dfe9eb65b017f344cdb6c9b
AKDs-Sandbox <furkanyasinakdag@gmial.com>
2025-04-08 13:19:29 +0200

    Test Commit

---
+++ src/main/java/fh/dualo/BackendSWT/BackendSwtApplication.java
@@ -0,0 +1,13 @@
+package fh.dualo.BackendSWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fh/dualo/BackendSWT/BackendSwtApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.dualo.BackendSWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
