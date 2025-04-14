27a7467725140f7a7e3dc45e1bc2b7b614a7bac6
AKDs-Sandbox <furkanyasinakdag@gmail.com>
2025-04-14 23:01:38 +0200

    Multi Module Hirachie und dependency Management fürs erste komplett fertig. Test Code für ein Mqtt Broker um etwas zum laufen zu haben

---
+++ SpringMQTConnector/src/main/java/fh/duao/MqttBroker/ApplicationServer.java
@@ -0,0 +1,10 @@
+package fh.duao.MqttBroker;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttBrokerApplication.class, args);
+    }
+}
--- SpringMQTConnector/src/main/java/fh/duao/MqttBroker/MqttBrokerApplication.java
+++ SpringMQTConnector/src/main/java/fh/duao/MqttBroker/MqttBrokerApplication.java
@@ -1,13 +1,41 @@
 package fh.duao.MqttBroker;
 
-import org.springframework.boot.SpringApplication;
+import io.moquette.broker.Server;
+import io.moquette.broker.config.MemoryConfig;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class MqttBrokerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MqttBrokerApplication.class, args);
-	}
+    private Server mqttBroker;
 
+    @PostConstruct
+    public void startBroker() {
+        mqttBroker = new Server();
+        Properties brokerProperties = new Properties();
+        brokerProperties.setProperty("port", "1883");
+        brokerProperties.setProperty("host", "0.0.0.0");
+        brokerProperties.setProperty("persistent_store", "moquette_store.mapdb");
+
+        try {
+            mqttBroker.startServer(new MemoryConfig(brokerProperties));
+            System.out.println("MQTT Broker started successfully!");
+        } catch (IOException e) {
+            System.err.println("Failed to start MQTT Broker: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @PreDestroy
+    public void stopBroker() {
+        if (mqttBroker != null) {
+            System.out.println("Stopping MQTT Broker...");
+            mqttBroker.stopServer();
+        }
+    }
 }
