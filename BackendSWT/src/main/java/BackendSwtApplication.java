062b0eba6584b2c410432993bc2d338e5ebe17ff
Noah Arendt <noah.arendt001@stud.fh-dortmund.de>
2025-04-23 09:34:36 +0000

    Edit BackendSwtApplication.java

--- BackendSWT/src/main/java/BackendSwtApplication.java
+++ BackendSWT/src/main/java/BackendSwtApplication.java
@@ -1,7 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(classes = BackendSwtApplication.class)
 public class BackendSwtApplication {
 
 	public static void main(String[] args) {
