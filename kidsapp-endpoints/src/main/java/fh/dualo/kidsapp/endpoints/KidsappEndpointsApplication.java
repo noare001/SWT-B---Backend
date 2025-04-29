8eb986d598b08ef59f3002c4f5eb0485c96e77e8
Furkan Akdag <furkan.akdag001@stud.fh-dortmund.de>
2025-04-29 12:12:36 +0200

    Setup Endpoints project

---
+++ kidsapp-endpoints/src/main/java/fh/dualo/kidsapp/endpoints/KidsappEndpointsApplication.java
@@ -0,0 +1,13 @@
+package fh.dualo.kidsapp.endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsappEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsappEndpointsApplication.class, args);
+	}
+
+}
