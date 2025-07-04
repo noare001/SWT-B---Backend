b6bf3794931ef18e2a98f9588f996b6696431a66
henrik <henrik.abtmeyer001@stud.fh-dortmund.de>
2025-07-04 14:30:08 +0200

    Cors Configs hinzugefügt, damit Backend Frontend Server akzeptiert (localhost:5173)

--- kidsapp-application/src/main/java/fh/dualo/kidsapp/application/KidsappEndpointsApplication.java
+++ kidsapp-application/src/main/java/fh/dualo/kidsapp/application/KidsappEndpointsApplication.java
@@ -2,12 +2,22 @@ package fh.dualo.kidsapp.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class KidsappEndpointsApplication {
+public class KidsappEndpointsApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KidsappEndpointsApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/api/**")
+				.allowedOrigins("http://localhost:5173")
+				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+				.allowCredentials(true);
+	}
 }
