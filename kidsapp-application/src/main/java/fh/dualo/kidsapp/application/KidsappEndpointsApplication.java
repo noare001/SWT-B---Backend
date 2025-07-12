9b397accece3ae845a13ab72d7a7483ea23b4618
AKDs-Sandbox <furkanyasinakdag@gmail.com>
2025-07-13 00:12:56 +0200

    cors

--- kidsapp-application/src/main/java/fh/dualo/kidsapp/application/KidsappEndpointsApplication.java
+++ kidsapp-application/src/main/java/fh/dualo/kidsapp/application/KidsappEndpointsApplication.java
@@ -16,7 +16,7 @@ public class KidsappEndpointsApplication implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
 				.addMapping("/api/**")
-				.allowedOrigins("http://localhost:5173", "http://kidsapp-frontend:80", "*")
+				.allowedOrigins("http://localhost:5173", "http://localhost:8080")
 				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
 				.allowCredentials(true);
 	}
